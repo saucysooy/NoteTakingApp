d95e0b8334e15def3bc1e6ac18f9465ac9236d7a
saucysooy <saucysooy@hotmail.com>
2024-11-07 23:23:55 +0100

    feat: Add detailed note screen
    Introduces user a more detailed view of the specified note
    
    Detailed note screen includes:
    - "Note Details" title header
    - Note's title
    - Note's body
    - "Back","Edit", and "Delete" Button

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.example.noteapp
 
-import android.annotation.SuppressLint
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -23,6 +21,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -39,13 +38,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.noteapp.ui.theme.NoteAppTheme
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.style.TextAlign
 
 
 class MainActivity : ComponentActivity() {
@@ -176,24 +174,24 @@ fun NewNoteScreen(modifier: Modifier = Modifier){
                 placeholder = { Text(text = "Type your note here...") }
             )
             Box (modifier = Modifier.fillMaxHeight(0.55f), contentAlignment = Alignment.BottomCenter) {
-                PairOfButtons("Create")
+                PairOfButtons("Cancel", "Create")
             }
         }
     }
 }
 
 @Composable
-fun PairOfButtons(action: String, modifier: Modifier = Modifier) {
+fun PairOfButtons(firstAction: String, secondAction: String, modifier: Modifier = Modifier) {
     Row (modifier = Modifier.fillMaxWidth()) {
         Button (onClick = { /*TODO*/ }, modifier = Modifier
             .weight(1f)
             .padding(horizontal = 16.dp)) {
-            Text(text = "Cancel")
+            Text(text = firstAction)
         }
         Button (onClick = { /*TODO*/ }, modifier = Modifier
             .weight(1f)
             .padding(horizontal = 16.dp)) {
-            Text(text = action)
+            Text(text = secondAction)
         }
     }
 }
@@ -234,8 +232,60 @@ fun EditScreen(currentTitle: String, currentBody: String, modifier: Modifier = M
                 placeholder = { Text(text = "Type your note here...") }
             )
             Spacer(modifier = Modifier.height(32.dp))
-            PairOfButtons("Save")
+            PairOfButtons("Cancel", "Save")
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NoteDetails(modifier: Modifier = Modifier) {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Box(modifier = Modifier.fillMaxWidth(0.95f)) {
+                        Text(text = "Note Details", modifier = Modifier.align(Alignment.Center),
+                            textAlign = TextAlign.Center,fontWeight = FontWeight(800))
+                    }
+                },
+            )
+        },
+        bottomBar = {
+            BottomAppBar {
+                Button (onClick = { /*TODO*/ }, modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 16.dp)) {
+                    Text("Delete")
+                }
+            }
+        }
+    ){ paddingValues ->
+        Box(modifier = Modifier.padding(paddingValues).fillMaxSize()) {
+            Column (modifier = Modifier.fillMaxWidth(0.9f).padding(start = 16.dp)) {
+                Text(text = "Note Title", fontWeight = FontWeight(800))
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = "Note Body")
+                Spacer(modifier = Modifier.height(16.dp))
+                PairOfButtons("Back", "Edit")
+            }
+        }
+    }
+}
+
+@Preview(
+    name = "Light Mode",
+    showBackground = true,
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "Dark Mode",
+    showBackground = true,
+)
+@Composable
+fun NoteDetailsPreview() {
+    NoteAppTheme {
+        NoteDetails()
     }
 }
 
@@ -267,7 +317,7 @@ fun EditScreenPreview() {
 @Composable
 fun PairOfButtonsPreview() {
     NoteAppTheme {
-        PairOfButtons("Save")
+        PairOfButtons("Cancel", "Save")
     }
 }
 
