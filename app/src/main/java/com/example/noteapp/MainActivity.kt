d7f8696489415af1f2ad3ded9e7eaaf5cf12b4b8
saucysooy <saucysooy@hotmail.com>
2024-11-07 14:14:28 +0100

    feat: Add Floating Action Button for creating new notes and a create note screen
    This introduces a Floating Action Button (FAB) to the note overview screen, and a screen for creating new notes
    
    The FAB is positioned in the bottom right and displays "+", making it intuitive for users to create new notes
    
    Create new note screen includes:
    - Create new note title
    - Title text field
    - Note's body/description text field

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -6,34 +6,40 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.unit.dp
-import com.example.noteapp.ui.theme.NoteAppTheme
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.noteapp.ui.theme.NoteAppTheme
+import androidx.compose.ui.text.input.TextFieldValue
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -70,31 +76,13 @@ fun NoteOverviewContent(modifier: Modifier = Modifier) {
             NoteCard()
         }
         Spacer(modifier = Modifier.height(16.dp))
-        Row {
-            NoteCard()
-            Spacer(modifier = Modifier.width(16.dp))
-            NoteCard()
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Row {
-            NoteCard()
-            Spacer(modifier = Modifier.width(16.dp))
-            NoteCard()
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Row {
-            NoteCard()
-            Spacer(modifier = Modifier.width(16.dp))
-            NoteCard()
-        }
-
     }
 }
 
 @Composable
 fun NoteOverview(modifier: Modifier = Modifier) {
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally, // Center header in NoteOverview
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
             .fillMaxWidth()
             .fillMaxHeight()
@@ -105,7 +93,9 @@ fun NoteOverview(modifier: Modifier = Modifier) {
         Box(modifier = Modifier.weight(2f)) {
             NoteOverviewContent()
         }
+        CreateButton(onClick = {})
     }
+
 }
 
 @Composable
@@ -136,13 +126,61 @@ fun NoteCard (modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun NoteCardPreview(){
-    NoteAppTheme {
-        Surface() {
-            NoteCard()
+fun CreateButton(onClick: () -> Unit) {
+    Box (modifier = Modifier.fillMaxWidth()) {
+        FloatingActionButton(
+            modifier = Modifier.align(Alignment.BottomEnd).padding(16.dp),
+            onClick = { onClick() },
+        ) {
+            Icon(Icons.Filled.Add, "Floating action button.")
         }
+    }
+}
+
+@Composable
+fun NewNoteScreen(modifier: Modifier = Modifier){
+    Surface(modifier = Modifier.fillMaxSize(),) {
+        Column (horizontalAlignment = Alignment.CenterHorizontally){
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(text = "Create New Note", fontWeight = FontWeight(800), fontSize = 25.sp)
+            Text(text = "")
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextField(
+                singleLine = true,
+                value = "",
+                onValueChange = {},
+                placeholder = { Text(text = "Enter Title...") }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextField(
+                modifier = Modifier.fillMaxHeight(0.6f).fillMaxWidth(0.9f),
+                singleLine = false,
+                value = "",
+                onValueChange = {},
+                placeholder = { Text(text = "Type your note here...") }
+            )
+        }
+    }
+}
+
+@Preview(
+    name = "Light Mode",
+    showBackground = true,
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "Dark Mode",
+    showBackground = true,
+)
+@Composable
+fun CreateScreenPreview() {
+    NoteAppTheme {
+        NewNoteScreen()
     }
 }
 
